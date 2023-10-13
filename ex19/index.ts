e40ef26ffaf32936a26469debcbedf2de0661003
afzal448 <140246788+afzal448@users.noreply.github.com>
2023-10-14 03:19:56 +0500

    Add files via upload

---
+++ ex19/index.ts
@@ -0,0 +1,6 @@
+/* Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.*/
+// Importing Guests List from exercise 15
+import{ guestList } from '../Ex15'
+
+console.log(`\nPrinting message indicating number of guests`)
+console.log(`We had finally invited ${guestList.length} guest in Ex 15.`)
