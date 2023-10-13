34cbf700b0711b034f63521f81ac9c7497dd836d
afzal448 <140246788+afzal448@users.noreply.github.com>
2023-10-14 03:14:27 +0500

    Add files via upload

---
+++ ex7/index.ts
@@ -0,0 +1,13 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+// Addition
+console.log("addition result: ",  5 + 3);
+
+// Subtraction
+console.log("Subtraction result:",   16 - 8); 
+
+// Multiplication
+console.log("Multiplication result:",  2 * 4);
+
+// Division
+console.log("Division result:" , 64/8); 
