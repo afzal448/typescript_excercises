d0a43eb154421f5a2ec740646dc7c48107c86d02
afzal448 <140246788+afzal448@users.noreply.github.com>
2023-10-14 03:15:06 +0500

    Add files via upload

---
+++ ex10/index.ts
@@ -0,0 +1,19 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// Addition
+console.log("addition result: ",  5 + 3);
+
+// line 4 performs addition operation
+
+// Subtraction
+console.log("Subtraction result:",   16 - 8); 
+
+// Multiplication
+console.log("Multiplication result:",  2 * 4);
+
+
+/* line 10 performs multiplication operation*/
+
+
+// Division
+console.log("Division result:" , 64/8); 
